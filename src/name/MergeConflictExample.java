858e95cedfbb4648a3067005ec793c83d1d83331
aeendale <aeendale@aeendale-PC.hsd1.va.comcast.net>
2020-04-02 13:04:52 -0400

    Commit from Abe

---
+++ src/name/MergeConflictExample.java
@@ -0,0 +1,13 @@
+package name;
+
+public class MergeConflictExample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void someMethod() {
+		System.out.println("Abe was here");
+	}
+
+}
